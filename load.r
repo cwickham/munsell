040c5303e98f9c333379ad1cb61f0e98c1182dff
Charlotte Macbook <cwickham@gmail.com>
2009-08-17 09:12:30 -0700

    got quotes

--- load.r
+++ load.r
@@ -8,7 +8,3 @@ PATH <- dirname(FILE)
 
 lapply(dir(file.path(PATH, "R"), full.name=T), source)
 load("data/munsell_map.rdata")
-
-#### munsell are viewed under illuminant C
-### rgb in R is under illuminant D65??
-
