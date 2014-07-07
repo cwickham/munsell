2a41334cb27ff043613f495b89bb4fa95bc525cf
Charlotte Wickham <cwickham@gmail.com>
2014-07-07 11:18:22 -0700

    added mnsl2hvc (inverse to hvc2mnsl)

--- R/convert.r
+++ R/convert.r
@@ -40,7 +40,8 @@ mnsl2hex <- mnsl
 #' this package value should be an integer in 0:10 and chroma an even number
 #' at most 24.  Note that not all possible specifications result in 
 #' representable colours.  Regular recycling rules apply.
-#' @param hue a character vector of Munsell hues
+#' @param hue a character vector of Munsell hues, or a 3 column data frame
+#' containing the hue value and chroma levels
 #' @param value a numeric vector of values
 #' @param chroma a numeric vector of chromas
 #' @param ... passed on to \code{\link{check_mnsl}}. Use \code{fix = TRUE} to
@@ -53,12 +54,49 @@ mnsl2hex <- mnsl
 #' # All values of 5PB with chroma 10
 #' hvc2mnsl("5PB", 1:9, 10) # note some are undefined
 #' plot_mnsl(hvc2mnsl("5PB", 1:9, 2))
-hvc2mnsl <- function(hue, value, chroma, ...){
-  selected <- paste(hue, " ", value, "/", chroma,  sep = "")
+hvc2mnsl <- function(hue, value = NULL, chroma = NULL, ...){
+  if(!(is.null(value) == is.null(chroma))) stop("specify both value and chroma")
+  hcv <- hue
+  if(!is.null(value)) {
+    hcv <- cbind(hcv, value, chroma)
+  }
+  selected <- paste(hcv[, 1], " ", hcv[, 2], "/", hcv[, 3],  sep = "")
   selected <- check_mnsl(selected, ...)
   selected
 }
 
+#' Converts a Munsell colour to a hue, chroma and value triplet
+#'
+#' Takes a text specification of a Munsell colour and returns
+#' the hue, chroma and value triplet.
+#'
+#' Munsell colours are specified by hue, value and chroma.  They 
+#' take a form like "5PB 5/10" where the first characters represent the
+#' hue, followed by a space then the value and chroma separated by a "/". In
+#' this package value should be an integer in 0:10 and chroma an even number
+#' at most 24.  Note that not all possible specifications result in 
+#' representable colours. 
+#' @param col a character vector of Munsell colours
+#' @param ... passed on to \code{\link{check_mnsl}}. Use \code{fix = TRUE} to
+#' fix "bad" colours
+#' @return a data frame with named columns hue, value and chroma containing the hue,
+#' value and chroma levels.
+#' @seealso \code{\link{check_mnsl}}, \code{\link{hvc2mnsl}}
+#' @export
+#' @examples
+#' mnsl2hvc("5PB 5/10")
+#' hvc2mnsl(mnsl2hvc("5PB 5/10"))
+mnsl2hvc <- function(col, ...){
+  col <- check_mnsl(col, ...)
+  col.split <- lapply(strsplit(col, "/"), 
+    function(x) unlist(strsplit(x, " ")))
+  col_mat <- data.frame(do.call(rbind, col.split),
+    stringsAsFactors = FALSE)
+  colnames(col_mat) <- c("hue", "value", "chroma")
+  col_mat[, "value"] <- as.numeric(col_mat[, "value"])
+  col_mat[, "chroma"] <- as.numeric(col_mat[, "chroma"])
+  col_mat
+}
 #' Converts an sRGB colour to Munsell
 #'
 #' Finds the closest Munsell colour (in LUV space) to the specified sRGB colour
@@ -86,3 +124,18 @@ rgb2mnsl <- function(R, G = NULL, B = NULL){
     else closest <- apply(dists, 1, which.min)
     munsell.map[closest, "name"]
 }
+
+RGB2mnsl <- function(rgb.cols){
+  LUV.vals <- as(rgb.cols, "LUV")@coords
+  # check for black
+  if (any(LUV.vals[,"L"] == 0)){
+    LUV.vals[LUV.vals[,"L"] == 0, ] <- 0
+  }
+  ncolors <- nrow(LUV.vals)
+  dist.calc <- function(x, y) rowSums((rep(x, each = ncolors) - y) ^ 2)
+  dists <- apply(munsell.map[, c("L", "U", "V")], 1, dist.calc, y = LUV.vals)
+  if(is.null(dim(dists)))  closest <- which.min(dists)
+  else closest <- apply(dists, 1, which.min)
+  munsell.map[closest, "name"]
+}
+
